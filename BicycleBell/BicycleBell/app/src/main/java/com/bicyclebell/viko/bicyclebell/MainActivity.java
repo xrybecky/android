ff4126f4b092a51af0583222f85313ba523fedd2
Viktor <rybeckyv@gmail.com>
2017-06-29 21:06:34 +0200

    main button animation + playing sound from another thread

--- BicycleBell/BicycleBell/app/src/main/java/com/bicyclebell/viko/bicyclebell/MainActivity.java
+++ BicycleBell/BicycleBell/app/src/main/java/com/bicyclebell/viko/bicyclebell/MainActivity.java
@@ -1,23 +1,26 @@
 package com.bicyclebell.viko.bicyclebell;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.TextView;
-import android.support.design.widget.FloatingActionButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context;
-
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -27,20 +30,40 @@ public class MainActivity extends AppCompatActivity {
         // Activate FontAwesome
         setAwesomeTypeFace();
 
-        // Media player
-        final MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.bicycle_bell_12);
-
+        // Button
         Button button = (Button) findViewById(R.id.main_ring_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mediaPlayer.start();
+
+                // Threading
+                new Thread(new Runnable() {
+                    public void run() {
+
+                        // Media player
+                        if(mediaPlayer != null){
+                            mediaPlayer.stop();
+                            mediaPlayer.release();
+                            mediaPlayer = null;
+                        }
+
+                        mediaPlayer = MediaPlayer.create(context, R.raw.bicycle_bell_07);
+                        mediaPlayer.start();
+                    }
+                }).start();
+
             }
         });
 
+        // Set Button animation
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(context,
+                R.animator.button_pulse);
+        set.setTarget(button);
+        set.start();
+
     }
 
-    private void setAwesomeTypeFace(){
+    private void setAwesomeTypeFace() {
         Typeface fontAwesome = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
 
         // Main Ring Button
